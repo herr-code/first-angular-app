e3c5ad2ff0376ee35c62be501a33fa1dee336ddb
Jhon Doe <johndoe@example.com>
2024-11-20 13:46:24 -0600

    create housing location component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+// In home.component.ts, import HousingLocationComponent by adding this line to the file level imports.
+import {HousingLocationComponent} from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  // Next update the imports property of the @Component metadata by adding HousingLocationComponent to the array.
+  imports: [HousingLocationComponent],
   template: `
     <!-- In home.component.ts, in @Component, update the template property with this code. -->
     <section>
@@ -10,6 +13,9 @@ import { Component } from '@angular/core';
         <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
       </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
