f82be76053e328aa203a7350df97c656542def17
Jhon Doe <johndoe@example.com>
2024-11-20 14:50:22 -0600

    add inputs to components

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -11,5 +12,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-
+  // You have to add the ! because the input is expecting the value to be passed. In this case, there is no default value.
+  // In our example application case we know that the value will be passed in - this is by design. 
+  // The exclamation point is called the non-null assertion operator and it tells the TypeScript compiler that the value of this property won't be null or undefined.
+  @Input() housingLocation!: HousingLocation;
 }
