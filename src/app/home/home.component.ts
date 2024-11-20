a1cf0943584abd88f3f62f9efe6549c3c0e034e5
Jhon Doe <johndoe@example.com>
2024-11-20 14:39:28 -0600

    create an interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 // In home.component.ts, import HousingLocationComponent by adding this line to the file level imports.
-import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+// In src/app/home/home.component.ts, add this import statement after the existing import statements so that HomeComponent can use the new interface.
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -21,5 +23,17 @@ import {HousingLocationComponent} from '../housing-location/housing-location.com
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
+  // In src/app/home/home.component.ts, replace the empty export class HomeComponent {} definition with this code to create a single instance of the new interface in the component.
+  //  If the data didn't satisfy the description of the interface, the IDE has enough information to give us helpful errors.
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
---
+++ src/app/housinglocation.ts
@@ -0,0 +1,11 @@
+// This step adds the properties to the interface that your app needs to represent a housing location.
+export interface HousingLocation {
+    id: number;
+    name: string;
+    city: string;
+    state: string;
+    photo: string;
+    availableUnits: number;
+    wifi: boolean;
+    laundry: boolean;
+}
