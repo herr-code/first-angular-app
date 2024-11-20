1fa6fd5fc1da1d46e056a8e9684f8e86579d8a70
Jhon Doe <johndoe@example.com>
2024-11-20 10:57:38 -0600

    hello world!

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template: `
+    <!-- In app.component.ts, in the @Component definition, replace the template line with this code to change the text in the app component. -->
+    <h1>Hello world!</h1>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  // In app.component.ts, in the AppComponent class definition, replace the title line with this code to change the component title.
+  title = 'homes';
+}
